7f5cd5bf8f0aee3489a133c6eb8546d1bbe73fd7
Janusz <janusz.niemiec@mail.com>
2026-05-08 16:18:37 -0700

    feat: update formula to v0.8.5 with first-run voice setup

--- Formula/c2.rb
+++ Formula/c2.rb
@@ -2,11 +2,11 @@ class C2 < Formula
   desc "TUI command-and-control app for LLMs with text and voice interaction"
   homepage "https://github.com/jrniemiec/c2"
   license "MIT"
-  version "0.8.4"
+  version "0.8.5"
 
   on_arm do
     url "https://github.com/jrniemiec/c2/releases/download/v#{version}/c2-v#{version}-darwin-arm64.tar.gz"
-    sha256 "99df3a571e0a8ee89663c58c2bfa1772bfeeff05c6b14442334ad94d0c9ac77b"
+    sha256 "6ae75dc17d2c372be9d5d9fc62fa6855771f7a24ab19e6a1301343b9c40278c1"
   end
 
   depends_on "portaudio"
