9cde1a8ae1a2fad793c8fb77acacff8a86e95d57
Janusz <janusz.niemiec@mail.com>
2026-05-19 08:25:25 -0700

    c2 0.9.1

--- Formula/c2.rb
+++ Formula/c2.rb
@@ -2,11 +2,11 @@ class C2 < Formula
   desc "TUI command-and-control app for LLMs with text and voice interaction"
   homepage "https://github.com/jrniemiec/c2"
   license "MIT"
-  version "0.9.0"
+  version "0.9.1"
 
   on_arm do
     url "https://github.com/jrniemiec/c2/releases/download/v#{version}/c2-v#{version}-darwin-arm64.tar.gz"
-    sha256 "d4c805cbb9dbad83fc8911663e04a49701df774f4a193f74a71e31f3e84685b5"
+    sha256 "dcf41bb547c042bda7f738e1a912ec7f615a9f7a1eed72a9ebeeccde6089dd00"
   end
 
   depends_on "portaudio"
